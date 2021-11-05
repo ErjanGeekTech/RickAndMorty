e6a100b9936cfa5e515199f60fa61f2829b5ef49
ErjanGeekTech <erjandyushebaev875@gmail.com>
2021-11-05 19:44:42 +0600

    remove arrayOf and remove 18 life of code

--- app/src/main/java/com/example/rickandmorty/data/local/db/AppDatabase.kt
+++ app/src/main/java/com/example/rickandmorty/data/local/db/AppDatabase.kt
@@ -1,19 +1,18 @@
-package com.example.rickandmorty.data.db
+package com.example.rickandmorty.data.local.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.example.rickandmorty.data.db.daos.CharactersDao
-import com.example.rickandmorty.data.db.daos.EpisodesDao
-import com.example.rickandmorty.data.db.daos.LocationDao
+import com.example.rickandmorty.data.local.db.daos.CharactersDao
+import com.example.rickandmorty.data.local.db.daos.EpisodesDao
+import com.example.rickandmorty.data.local.db.daos.LocationDao
 import com.example.rickandmorty.models.RickAndMortyCharacters
 import com.example.rickandmorty.models.RickAndMortyEpisodes
 import com.example.rickandmorty.models.RickAndMortyLocations
 
-@Database(entities = arrayOf(RickAndMortyCharacters::class, RickAndMortyLocations::class, RickAndMortyEpisodes::class), version = 2)
+@Database(entities = [RickAndMortyCharacters::class, RickAndMortyLocations::class, RickAndMortyEpisodes::class], version = 2)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun characterDao(): CharactersDao
     abstract fun episodeDao(): EpisodesDao
     abstract fun locationDao(): LocationDao
-
 }
