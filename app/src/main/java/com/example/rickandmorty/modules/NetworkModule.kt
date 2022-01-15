3f0fd650fa83523291de4a03eca83066b91cc5fc
ErjanGeekTech <erjandyushebaev875@gmail.com>
2022-01-15 14:37:35 +0600

    change koin on hilt

--- app/src/main/java/com/example/rickandmorty/modules/NetworkModule.kt
+++ app/src/main/java/com/example/rickandmorty/modules/NetworkModule.kt
@@ -1,12 +1,28 @@
 package com.example.rickandmorty.modules
 
-import com.example.rickandmorty.data.network.RetrofitClient
-import org.koin.dsl.module
+import com.example.rickandmorty.data.remote.RetrofitClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
 
-val networkModule = module {
-    single { RetrofitClient() }
-    single { get<RetrofitClient>().provideCharacterApiService() }
-    single { get<RetrofitClient>().provideLocationApiService() }
-    single { get<RetrofitClient>().provideEpisodeApiService() }
+    @Singleton
+    val retrofitClient = RetrofitClient()
+
+    @Singleton
+    @Provides
+    fun provideCharacterApiService() = retrofitClient.provideCharacterApiService()
+
+    @Singleton
+    @Provides
+    fun provideEpisodeService() = retrofitClient.provideEpisodeApiService()
+
+    @Singleton
+    @Provides
+    fun provideLocationApiService() = retrofitClient.provideLocationApiService()
 }
