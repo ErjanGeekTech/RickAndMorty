25811c1033d61b666cb899d7e72d211f38f98cca
ErjanGeekTech <erjandyushebaev875@gmail.com>
2022-01-15 14:36:18 +0600

    add progress bar

--- app/src/main/java/com/example/rickandmorty/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/rickandmorty/presentation/ui/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.rickandmorty.ui.activity
+package com.example.rickandmorty.presentation.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -6,12 +6,13 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.rickandmorty.R
 import com.example.rickandmorty.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private val binding by viewBinding(ActivityMainBinding::bind)
@@ -20,7 +21,12 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initialize()
         setupNavigation()
+    }
+
+    private fun initialize() {
+        binding.bottomNavigation.itemIconTintList = null
     }
 
     private fun setupNavigation() {
@@ -34,8 +40,8 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             R.id.locationsFragment
         ).build()
 
-        setupActionBarWithNavController(this, navController, appBarConfiguration)
         setupWithNavController(binding.bottomNavigation, navController)
+        setupWithNavController(binding.toolbar, navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
