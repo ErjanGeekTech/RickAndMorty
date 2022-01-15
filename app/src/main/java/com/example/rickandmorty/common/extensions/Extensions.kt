ba088e3e1b5e17ac09b8274d65b37791db9f5e40
ErjanGeekTech <erjandyushebaev875@gmail.com>
2022-01-15 14:31:54 +0600

    add capitalized

--- app/src/main/java/com/example/rickandmorty/common/extensions/Extensions.kt
+++ app/src/main/java/com/example/rickandmorty/common/extensions/Extensions.kt
@@ -5,6 +5,7 @@ import android.net.ConnectivityManager
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
+import java.util.*
 
 fun Fragment.verifyAvailableNetwork(): Boolean {
     val connectivityManager =
@@ -17,3 +18,11 @@ fun ImageView.setImage(url: String) =
     Glide.with(this)
         .load(url)
         .into(this)
+
+fun String.capitalized(): String {
+    return this.replaceFirstChar {
+        if (it.isLowerCase())
+            it.titlecase(Locale.getDefault())
+        else it.toString()
+    }
+}
