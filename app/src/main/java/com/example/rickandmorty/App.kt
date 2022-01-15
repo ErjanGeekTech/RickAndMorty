dce52a60c3bd3e1beaff88d1ae3ef6cc2c1849a9
ErjanGeekTech <erjandyushebaev875@gmail.com>
2022-01-15 14:03:54 +0600

    change koin on hilt

--- app/src/main/java/com/example/rickandmorty/App.kt
+++ app/src/main/java/com/example/rickandmorty/App.kt
@@ -1,27 +1,7 @@
 package com.example.rickandmorty
 
 import android.app.Application
-import com.example.rickandmorty.modules.networkModule
-import com.example.rickandmorty.modules.repositoryModule
-import com.example.rickandmorty.modules.vMModule
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.GlobalContext.startKoin
+import dagger.hilt.android.HiltAndroidApp
 
-class App : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-
-        //Start Koin
-        startKoin {
-            androidContext(this@App)
-            androidLogger()
-            modules(listOf(
-                vMModule,
-                repositoryModule,
-                networkModule
-            ))
-        }
-    }
-}
+@HiltAndroidApp
+class App : Application()
