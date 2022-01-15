8952688eb920637a817f4e0a332f63af4eca6186
ErjanGeekTech <erjandyushebaev875@gmail.com>
2022-01-15 14:19:45 +0600

    update fetchCharacters and for request fetchCharacter add Resource.kt state

--- app/src/main/java/com/example/rickandmorty/data/repositories/CharacterRepository.kt
+++ app/src/main/java/com/example/rickandmorty/data/repositories/CharacterRepository.kt
@@ -1,40 +1,20 @@
 package com.example.rickandmorty.data.repositories
 
-import androidx.lifecycle.MutableLiveData
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
-import com.example.rickandmorty.data.network.apiservice.CharacterApiService
-import com.example.rickandmorty.data.repositories.pagingSource.CharacterPagingSource
-import com.example.rickandmorty.models.RickAndMortyCharacters
+import com.example.rickandmorty.base.BaseRepository
+import com.example.rickandmorty.common.resource.Resource
+import com.example.rickandmorty.data.remote.apiservices.CharacterApiService
+import com.example.rickandmorty.data.remote.dtos.RickAndMortyCharacterDto
+import com.example.rickandmorty.data.remote.pagingsources.CharacterPagingSource
+import com.example.rickandmorty.domain.repositories.DetailCharacterRepository
 import kotlinx.coroutines.flow.Flow
-import retrofit2.Call
-import retrofit2.Response
+import javax.inject.Inject
 
-class CharacterRepository constructor(private var service: CharacterApiService) {
+class CharacterRepository @Inject constructor(private var service: CharacterApiService) :
+    BaseRepository(), DetailCharacterRepository {
 
-    fun fetchCharacters(): Flow<PagingData<RickAndMortyCharacters>> {
-        return Pager(config = PagingConfig(
-            pageSize = 10, enablePlaceholders = false
-        ), pagingSourceFactory = {
-            CharacterPagingSource(service)
-        }).flow
-    }
+    fun fetchCharacters() = doPagingRequest(CharacterPagingSource(service))
 
-    fun getCharacterId(id: Int? = null): MutableLiveData<RickAndMortyCharacters> {
-        val character: MutableLiveData<RickAndMortyCharacters> = MutableLiveData()
-        service.character(id).enqueue(object : retrofit2.Callback<RickAndMortyCharacters> {
-            override fun onResponse(
-                call: Call<RickAndMortyCharacters>,
-                response: Response<RickAndMortyCharacters>
-            ) {
-                character.value = response.body()
-            }
-
-            override fun onFailure(call: Call<RickAndMortyCharacters>, t: Throwable) {
-                character.value = null
-            }
-        })
-        return character
+    override fun fetchCharacter(id: Int): Flow<Resource<RickAndMortyCharacterDto>> = doRequest {
+        service.fetchCharacter(id)
     }
 }
