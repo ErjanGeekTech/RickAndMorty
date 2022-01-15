fc9da12d41fdf29663f29ce3166ab1d9c2affb20
ErjanGeekTech <erjandyushebaev875@gmail.com>
2022-01-15 14:38:13 +0600

    repository module(clean architecture)

--- app/src/main/java/com/example/rickandmorty/modules/RepositoryModule.kt
+++ app/src/main/java/com/example/rickandmorty/modules/RepositoryModule.kt
@@ -1,12 +1,28 @@
 package com.example.rickandmorty.modules
 
+import com.example.rickandmorty.data.remote.apiservices.CharacterApiService
+import com.example.rickandmorty.data.remote.apiservices.EpisodeApiService
 import com.example.rickandmorty.data.repositories.CharacterRepository
 import com.example.rickandmorty.data.repositories.EpisodeRepository
-import com.example.rickandmorty.data.repositories.LocationRepository
-import org.koin.dsl.module
+import com.example.rickandmorty.domain.repositories.DetailCharacterRepository
+import com.example.rickandmorty.domain.repositories.FirstEpisodeRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
-val repositoryModule = module {
-    factory { CharacterRepository(get()) }
-    factory { EpisodeRepository(get()) }
-    factory { LocationRepository(get()) }
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideCharacterRepository(service: CharacterApiService): DetailCharacterRepository =
+        CharacterRepository(service)
+
+    @Singleton
+    @Provides
+    fun provideLocationRepository(service: EpisodeApiService): FirstEpisodeRepository =
+        EpisodeRepository(service)
 }
